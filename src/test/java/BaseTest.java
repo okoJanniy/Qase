dacab46ab140ec5b7b7403fa4f26a3e666e94176
okoJanniy <82619523+okoJanniy@users.noreply.github.com>
2021-06-08 21:52:42 +0300

    Не получается найти локатор кнопки create project

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -13,8 +13,8 @@ public class BaseTest {
         Configuration.baseUrl = System.getenv().getOrDefault("QASE_URL", PropertyReader.getProperty("qase.url"));
         user = System.getenv().getOrDefault("QASE_USER", PropertyReader.getProperty("qase.user"));
         password = System.getenv().getOrDefault("QASE_PASSWORD", PropertyReader.getProperty("qase.password"));
-        Configuration.browser = "chrome"; //(пример.можно вписать любой браузер)
-        Configuration.clickViaJs = true;  //(клики Javascript)
+      //  Configuration.browser = "chrome"; //(пример.можно вписать любой браузер)
+      //  Configuration.clickViaJs = true;  //(клики Javascript)
         Configuration.headless = true;    //(без отрисовки в браузере,экономия)
         Configuration.startMaximized = true;   //(обязательно. развертка окна браузера)
         Configuration.timeout = 10000;         //(таймаут ожидания обязательно)
---
+++ src/test/java/ProjectTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Condition;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ProjectTest extends BaseTest {
+
+    @Test
+    public void project() {
+        open("/login");
+        $("#inputEmail").sendKeys(user);
+        $("#inputPassword").sendKeys(password);
+        $("#btnLogin").click();
+        $("#user-menu").shouldBe(Condition.visible);
+        $("#createButton").click();
+        $("#inputTitle").sendKeys("AntonProject");
+        $("#inputCode").sendKeys("AP");
+        $("#inputDescription").sendKeys("This my new project");
+        $("#public-access-type").click();
+        $(".submit").shouldBe(Condition.text("Create project")).click();
+        $(".col").click();
+        $(".defect-title").shouldBe(Condition.text("TestProject"));
+
+    }
+}
